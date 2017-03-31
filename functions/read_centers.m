61261420615dca6749f5217c49875ce2e34592f3
Vincent Maison <pro@vincent.maison>
2017-03-31 10:26:09 +0200

    Fix genericHots: 078e105

--- functions/read_centers.m
+++ functions/read_centers.m
@@ -1,13 +1,17 @@
-function [centers] = read_centers(filename)
+function [centers] = read_centers(filename, params)
 
 fid = fopen(filename, 'r');
 header = fgetl(fid);
 tmp = str2num(fgetl(fid));
 Ndim = tmp(1);
-NPols = tmp(2); 
 tmp = str2num(fgetl(fid));
 nCenters = tmp(1);
 nbFeats = tmp(2);
+if nCenters == params.nbCenters(1)
+    NPols = params.nbPols;
+else
+    NPols = params.nbCenters(find(nCenters==params.nbCenters)-1);
+end
 radius = (nbFeats/NPols-1)/2;
 centers = zeros(nCenters, NPols, nbFeats/NPols);
 for ind = 1:nCenters
