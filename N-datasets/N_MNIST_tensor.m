94c6034a8a34acdde19b980b2a8af0c3ba247d30
Vincent Maison <pro@vincent.maison>
2017-05-16 15:21:45 +0200

    fix tensor + test parfor

--- N-datasets/N_MNIST_tensor.m
+++ N-datasets/N_MNIST_tensor.m
@@ -15,31 +15,55 @@ max_ev_test = max(cellfun(@(ev) max(ev.ts), all_events_test(:,1)));
 
 
 max_ev = max([max_ev_train, max_ev_test]);
-w_gliss = max_ev/100;
 % hists_activity = cellfun(@(ev) compute_event_activity(ev, 1000, max_ev, 10), ...
 %   all_events_train(:,1), 'UniformOutput', 0);
-% 
+%
 % hold off;
 % for ind = 1:50
 %   plot(hists_activity{ind})
 %   hold on;
 % end
-% 
+%
 % cellfun(@(ev) draw_scene_ev2D(ev, 10000, 10000, [35 35]), ...
 %   all_events_train(:,1));
 
-% test = convert_events_into_tensor(all_events_test{1}, 4000, max_ev, [35, 35], 3);
+% test = convert_events_into_tensor(all_events_test{1}, 4000, max_ev, [34, 34], 3);
 % for ind = 1:size(test,3)
 %   imagesc(test(:,:,ind))
 %   pause
 % end
 
-train_features = cellfun(@(ev) reshape( ...
-  convert_events_into_tensor(ev, 4000, max_ev, [35, 35], 3) ...
-  ,1,[]), ...
-  all_events_train(:,1), 'UniformOutput', 0);
+size_spatial = [34,34];
+w_gliss_bins = 3;
+w_frame_us = 4000;
 
-test_features = cellfun(@(ev) reshape( ...
-  convert_events_into_tensor(ev, 4000, max_ev, [35, 35], 3) ...
-  ,1,[]), ...
-  all_events_test(:,1), 'UniformOutput', 0);
+N_workers = 72;
+myCluster=parcluster('local');
+myCluster.NumWorkers=N_workers;
+parpool(myCluster,N_workers)
+
+
+train_features = zeros(prod(size_spatial)*(ceil(max_ev/w_frame_us)-2*w_gliss_bins), ...
+    numel(all_events_train,1));
+test_features = zeros(prod(size_spatial)*(ceil(max_ev/w_frame_us)-2*w_gliss_bins), ...
+    numel(all_events_test,1));
+parfor ind = 1:numel(all_events_train,1)
+  train_features(:,ind) = reshape( convert_events_into_tensor(...
+    all_events_train{ind,1}, w_frame_us, max_ev, size_spatial, w_gliss_bins) ...
+    ,1,[]);
+end
+parfor ind = 1:numel(all_events_test,1)
+  test_features(:,ind) = reshape( convert_events_into_tensor(...
+    all_events_test{ind,1}, w_frame_us, max_ev, size_spatial, w_gliss_bins) ...
+    ,1,[]);
+end
+
+% train_features = cellfun(@(ev) reshape( ...
+%   convert_events_into_tensor(ev, w_frame_us, max_ev, size_spatial, w_gliss_bins) ...
+%   ,1,[]), ...
+%   all_events_train(:,1), 'UniformOutput', 0);
+%
+% test_features = cellfun(@(ev) reshape( ...
+%   convert_events_into_tensor(ev, w_frame_us, max_ev, size_spatial, w_gliss_bins) ...
+%   ,1,[]), ...
+%   all_events_test(:,1), 'UniformOutput', 0);
--- functions/convert_events_into_tensor.m
+++ functions/convert_events_into_tensor.m
@@ -5,10 +5,20 @@ ev_activity = zeros(boardSize(1), boardSize(2), nb_bins);
 curr_bin = 1;
 
 for ind = 1:numel(ev.ts)
+  if ev.p(ind) ~= 1
+    continue;
+  end
   while ev.ts(ind) > (curr_bin*timestep)
     curr_bin = curr_bin+1;
   end
-  ev_activity(ev.x(ind), ev.y(ind), curr_bin) = ev_activity(ev.x(ind), ev.y(ind), curr_bin) + 1;
+  try
+    ev_activity(ev.x(ind), ev.y(ind), curr_bin) = ev_activity(ev.x(ind), ev.y(ind), curr_bin) + 1;
+  catch
+     fprintf('numel events : %d\n', numel(ev.ts));
+     fprintf('curr ev : (%d, %d, %d, %d)\n', ev.ts(ind), ev.x(ind), ev.y(ind), ev.p(ind));
+     fprintf('timestep %d, finaltime %d, curr_bin %d\n', timestep, final_time, curr_bin);
+     error('index m8.');
+  end
 end
 
 if ~exist('normalize_bins', 'var')
