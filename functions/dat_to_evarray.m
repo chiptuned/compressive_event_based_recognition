6a3fbdfbfd8dd9c2ab814c587e0dd147f667e8c9
Vincent Maison <pro@vincent.maison>
2017-05-22 19:14:42 +0200

    draw_centers and end pipeline hots
    dat to evarray tbc

--- functions/dat_to_evarray.m
+++ functions/dat_to_evarray.m
@@ -1,1 +1,57 @@
-function [trainc, testc] = dat_to_evarray(ev_array_dat, labels, idx_pres);
+function [out_ev_array] = dat_to_evarray(out_dat, labels, idx_pres);
+% j'en veux plutot autant que out_dat
+%
+% cellev =  1×3 cell array     {8×3 cell}    {992×3 cell}    {1000×3 cell}
+%
+% cellev{1} = 8×3 cell array
+% [1×1 struct]    [ 4]    [39706]
+% [1×1 struct]    [ 1]    [50910]
+% [1×1 struct]    [ 6]    [33574]
+% [1×1 struct]    [ 7]    [41060]
+% [1×1 struct]    [ 6]    [26144]
+% [1×1 struct]    [10]    [44993]
+% [1×1 struct]    [ 4]    [ 9356]
+% [1×1 struct]    [ 5]    [51818]
+%
+%
+% >> cellev{1}{1} =  struct with fields:
+%      x: [4871×1 uint8]
+%      y: [4871×1 uint8]
+%      p: [4871×1 logical]
+%     ts: [4871×1 uint32]
+
+out_ev_array = cell(size(out_dat));
+for dat = 1:size(out_dat,2)
+
+  % find correspondancies (in no of events) for each struct
+  n_pr = numel(idx_pres{dat});
+  ev_start = zeros(1,n_pr);
+  ev_end = zeros(1,n_pr);
+  for ind = 1:n_pr
+    ev_start(ind) = find(out_dat{dat, 1}.ts == labels{dat}{ind,1}, 1)
+    ev_end(ind) = find(out_dat{dat, 1}.ts == labels{dat}{ind,2}, 1) - 1
+    labels{dat}(ind,1)
+    labels{dat}(ind,2)
+    out_dat{dat, 1}.ts(ev_start(ind))
+    out_dat{dat, 1}.ts(ev_end(ind))
+    pause
+  end
+
+  for layer = 1:size(out_dat,1)
+    out_ev_array{dat, layer} = cell(n_pr, 3);
+    % putting labels and ids in it
+    out_ev_array{dat, layer}(:,2) = labels{dat}(:,3);
+    out_ev_array{dat, layer}(:,3) = idx_pres{dat};
+    for pres = 1:n_pr
+      % faire l'histoire d'extraire les namefields etc
+      ev_struct.ts = out_dat{dat, layer}.ts(ev_start(pres):ev_end(pres)) ...
+        - labels{dat}(pres,1);
+      % for tous les autres namefields
+      ev_struct.ts = out_dat{dat, layer}.ts(ev_start(pres):ev_end(pres))
+      ev_struct.ts = out_dat{dat, layer}.ts(ev_start(pres):ev_end(pres))
+      ev_struct.ts = out_dat{dat, layer}.ts(ev_start(pres):ev_end(pres))
+      % voila.
+      out_ev_array{dat, layer}{pres,1} = ev_struct;
+    end
+  end
+end
--- functions/draw_centers.m
+++ functions/draw_centers.m
@@ -1,12 +1,9 @@
 function draw_centers(c, params)
 % only 2D
 
-MAX_SUBP_X = 6;
-MAX_SUBP_Y = 4;
-
 if isfield(c,'k')
   nbP = numel(c.k)-1;
-  new_C = (c.data*c.pca_eigv{nbP+1}(:,1:c.k{nbP+1})') + repmat(c.mu{nbP+1},size(c.data,1),1);
+  new_C = (c.data*c.proj{nbP+1}(:,1:c.k{nbP+1})') + repmat(c.mu{nbP+1},size(c.data,1),1);
   C_invproj = [];
   borne_inf = [1];
   borne_sup = [c.k{1}];
@@ -15,7 +12,7 @@ if isfield(c,'k')
     borne_sup = [borne_sup, borne_sup(ind-1)+c.k{ind}];
   end
   for ind = 1:nbP
-    C_invproj = [C_invproj, (new_C(:,borne_inf(ind):borne_sup(ind))*c.pca_eigv{ind}(:,1:c.k{ind})') ...
+    C_invproj = [C_invproj, (new_C(:,borne_inf(ind):borne_sup(ind))*c.proj{ind}(:,1:c.k{ind})') ...
       + repmat(c.mu{ind},size(c.data,1),1)];
   end
   radius = (sqrt(numel(C_invproj(ind,:))/nbP)-1)/2; % FIXME : should have this with parameters coz not 2D 100%
@@ -28,7 +25,7 @@ if isfield(c,'k')
   end
   c = permute(old_centers,[2 3 4 1]);
 end
-% 
+
 % figure;
 % nbC = size(c,4);
 % nbP = size(c,3);
@@ -49,22 +46,56 @@ end
 % end
 % drawnow;
 
-figure;
+% Nb of plots config. Min is 2*1=2 and Max is 6*4=24
+subp_y_choices = [1, 2, 2, 2, 3, 4, 5, 6];
+subp_x_choices = [2, 2, 3, 4, 5, 6, 6, 8];
+%pols_that_in_i = [2, 2, 3, 4, 5, 6, 6, 6];
 nbC = size(c,4);
 nbP = size(c,3);
-x_subp = mod(nbC,8);
-if x_subp == 0
-  x_subp = 8;
+
+MAX_SIZE_SUBP = 6; % Fix the maximum nb of plots in a fig
+
+subp_y_choices = subp_y_choices(1:MAX_SIZE_SUBP);
+subp_x_choices = subp_x_choices(1:MAX_SIZE_SUBP);
+
+nb_figs = ceil(nbC/subp_y_choices(end)/subp_x_choices(end));
+idx_subp_y_figs = ones(1,nb_figs);
+idx_subp_x_figs = ones(1,nb_figs);
+
+while sum(subp_x_choices(idx_subp_x_figs).*subp_y_choices(idx_subp_y_figs)) < nbC
+    [~, fig_to_inc] = min(subp_x_choices(idx_subp_x_figs).*subp_y_choices(idx_subp_y_figs));
+    idx_subp_x_figs(fig_to_inc) = idx_subp_x_figs(fig_to_inc)+1;
+    idx_subp_y_figs(fig_to_inc) = idx_subp_y_figs(fig_to_inc)+1;
 end
+nb_plots_figs = subp_x_choices(idx_subp_x_figs).*subp_y_choices(idx_subp_y_figs);
+
+sizey_center = floor(sqrt(nbP));
+sizex_center = ceil(nbP/sizey_center);
+rx = size(c,1);
+ry = size(c,2);
+
+indf = 0;
 for ind = 1:nbC
-  subplot(x_subp, ceil(nbC/8), ind)
-  img = [];
+  newindf = find(cumsum(nb_plots_figs) >= ind,1);
+  if newindf ~= indf
+    figure;
+  end
+  indf = newindf;
+  ind_in_subp = ind - sum(nb_plots_figs(1:indf-1));
+  subplot(subp_y_choices(idx_subp_y_figs(indf)), subp_x_choices(idx_subp_x_figs(indf)), ind_in_subp)
+  img = ones(sizey_center*size(c,2), sizex_center*size(c,1));
   for ind2 = 1:nbP
-    img = [img, c(:,:,ind2,ind)];
+    indcx = mod(ind2, sizex_center);
+    if indcx == 0
+      indcx = sizex_center;
+    end
+    indcy = ceil(ind2/sizex_center);
+    img((1:ry)+ry*(indcy-1), (1:rx)+rx*(indcx-1)) = c(:,:,ind2,ind);
   end
   imagesc(img);
   caxis([0 1])
   axis image
   colormap hot
+  title(num2str(ind))
+  %set(gca,'visible','off')
 end
-drawnow;
